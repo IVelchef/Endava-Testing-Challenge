1282c5e71ca4a3658aa15e6409f58bb0e6ef75fd
IVelchef <ivan.velchev.a61@learn.telerikacademy.com>
2025-01-05 16:16:51 +0200

    Add REST Assured project

---
+++ Reqres-REST_Assured_Endava_Challenge/src/test/java/baseTests/SingleUserDetailsTest.java
@@ -0,0 +1,15 @@
+package baseTests;
+import testData.TestData;
+import baseSutup.BaseSetup;
+import org.testng.annotations.Test;
+
+public class SingleUserDetailsTest extends BaseSetup {
+
+
+    @Test
+    public void testSingleUserDetails (){
+
+        singleUserDetails();
+
+    }
+}
